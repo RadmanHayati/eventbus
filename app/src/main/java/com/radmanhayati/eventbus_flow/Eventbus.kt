07cf8b91bf5f328fd561255a3a5c5d0197616d55
RADMAN_HAYATI <radman.hayatii@gmail.com>
2023-05-25 12:48:31 +0430

    Eventbus class created

--- app/src/main/java/com/radmanhayati/eventbus_flow/Eventbus.kt
+++ app/src/main/java/com/radmanhayati/eventbus_flow/Eventbus.kt
@@ -1,4 +1,21 @@
 package com.radmanhayati.eventbus_flow
 
-class Eventbus {
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.asSharedFlow
+
+class EventBus {
+
+    private val _events = MutableSharedFlow<AppEvent>()
+    val events = _events.asSharedFlow()
+
+    suspend fun invokeEvent(event: AppEvent) = _events.emit(event)
+
+    companion object{
+        val instance = EventBus()
+    }
+}
+
+enum class AppEvent {
+    LOGOUT,
+    LOGIN
 }
