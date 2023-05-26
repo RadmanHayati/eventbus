4bdaf531cd5f0eb44d7a91cb0c3b8b0ee18213c2
RADMAN_HAYATI <radman.hayatii@gmail.com>
2023-05-26 12:25:41 +0430

    Publisher side implemented

--- app/src/main/java/com/radmanhayati/eventbus_flow/LoginActivity.kt
+++ app/src/main/java/com/radmanhayati/eventbus_flow/LoginActivity.kt
@@ -1,22 +1,19 @@
 package com.radmanhayati.eventbus_flow
 
-import android.content.Context
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.flow.filter
 import kotlinx.coroutines.launch
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
         GlobalScope.launch {
-            EventBus.instance.events.filter { event -> event == AppEvent.LOGIN }.collectLatest {
-                Toast.makeText(applicationContext, "lksdj", Toast.LENGTH_SHORT).show()
-            }
+            EventBus.instance.invokeEvent(AppEvent.LOGIN)
         }
     }
 }
